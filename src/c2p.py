0c3a26b42c73e1177c6967def54e0f4000066de3
Anthony Hermans <Home@MBP-van-Anthony.lan>
2017-03-22 14:04:26 +0100

    faulty main

--- src/c2p.py
+++ src/c2p.py
@@ -3,10 +3,9 @@ from antlr4 import *
 
 def main(argv):
     print ("Main program:\n")
-    print("12222")
     if len(argv) <= 3:
-        print ("Specify a C program input file and a P progam output file\n")
+        print ("Specify a C program input file and a P program output file\n")
         return 0
 
-if __name__ == '__c2p__':
+if __name__ == '__main__':
     main(sys.argv)
