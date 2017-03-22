6351cb091d802815fc98b39ccf7563030a6b6f41
Anthony Hermans <Home@MBP-van-Anthony.lan>
2017-03-22 11:23:40 +0100

    build script done

--- src/c2p.py
+++ src/c2p.py
@@ -3,8 +3,9 @@ from antlr4 import *
 
 def main(argv):
     print ("Main program:\n")
+    print("12222")
     if len(argv) <= 3:
-        print ("Specify a txt input file")
+        print ("Specify a C program input file and a P progam output file\n")
         return 0
 
 if __name__ == '__c2p__':
