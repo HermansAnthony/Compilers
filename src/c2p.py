9ccf875fb4646095c13365194f159aa5b01e6800
Anthony Hermans <Home@MBP-van-Anthony.lan>
2017-03-22 14:06:32 +0100

    ...

--- src/c2p.py
+++ src/c2p.py
@@ -3,7 +3,7 @@ from antlr4 import *
 
 def main(argv):
     print ("Main program:\n")
-    if len(argv) <= 3:
+    if len(argv) < 3:
         print ("Specify a C program input file and a P program output file\n")
         return 0
 
