6129c444246de8848b07e51595ed57c0edf80b55
babbel056 <hermans.anthony@hotmail.com>
2017-04-25 17:18:51 +0200

    c2p.py minor change

--- src/c2p.py
+++ src/c2p.py
@@ -3,8 +3,8 @@ from antlr4 import *
 from BasicErrorListener import BasicErrorListener
 from CmmLexer import CmmLexer
 from CmmParser import CmmParser
-from astBuilder import AstBuilder  
-      
+from astBuilder import AstBuilder
+
 def main(argv):
     print ("Main program:\n")
     if len(argv) < 3:
@@ -12,19 +12,21 @@ def main(argv):
         return 0
     input = FileStream(argv[1])
     # Initialize the lexer, tokenstream and the parser with the input
-    lexer = CmmLexer(input) 
-    stream = CommonTokenStream(lexer) 
+    lexer = CmmLexer(input)
+    stream = CommonTokenStream(lexer)
     parser = CmmParser(stream)
 
     parser._listeners.append(BasicErrorListener())
-
     # Build the parse tree
     parseTree = parser.program()
 
     # Generate and visit the Abstract Syntax Tree
     visitor = AstBuilder()
     ast = visitor.visit(parseTree)
-    ast.toDot("AST")
+    filename = str(argv[1])
+    outputAST = "ast_output/" + (filename.split("/")[1]).split(".")[0] + ".dot"
+    print(outputAST)
+    ast.toDot(outputAST)
 
 if __name__ == '__main__':
     main(sys.argv)
