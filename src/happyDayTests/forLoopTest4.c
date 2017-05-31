96e5a9185a60a12c95442b23cb96107925deb779
babbel056 <hermans.anthony@hotmail.com>
2017-05-31 19:59:25 +0200

    More loop tests

---
+++ src/happyDayTests/forLoopTest4.c
@@ -0,0 +1,19 @@
+//  ********************************************
+//  Authors: Jeroen Verstraelen - Anthony Hermans
+//  Description: This testfile tests a regular forloop (with incrementing iterator)
+//  ********************************************
+#include <stdio.h>
+int main(){
+  int upperBound = 10;
+  for (int iterator = 0; iterator < upperBound; iterator++){
+    printf("This for loop is now at iteration %i\n", iterator);
+    if (iterator == 5){
+      printf("Loop ends at the iteration%d\n",iterator);
+      break;
+    }else{
+      continue;
+    }
+    printf("I should never reach this point\n");
+  }
+  printf("Loop ended\n");
+}
