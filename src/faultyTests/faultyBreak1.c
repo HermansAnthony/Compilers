d50e0caf7c74a990291e77b68df26c39441b9dd1
babbel056 <hermans.anthony@hotmail.com>
2017-05-31 10:47:45 +0200

    Semantic tests of break-continue

---
+++ src/faultyTests/faultyBreak1.c
@@ -0,0 +1,8 @@
+//  ********************************************
+//  Authors: Jeroen Verstraelen - Anthony Hermans
+//  Description: This testfile tests break statements
+//  Break outside loop
+//  ********************************************
+int main(){
+  break;
+}
