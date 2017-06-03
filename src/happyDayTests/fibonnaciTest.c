5d21be9c90e587a40402d466775c83b531d90cd3
babbel056 <hermans.anthony@hotmail.com>
2017-06-03 13:33:53 +0200

    The famous fibonnaci test :) with recursion

---
+++ src/happyDayTests/fibonnaciTest.c
@@ -0,0 +1,25 @@
+//  ********************************************
+//  Authors: Jeroen Verstraelen - Anthony Hermans
+//  Description: This program will give you the fibonnaci series with a specified input
+//  ********************************************
+#include <stdio.h>
+int fib(int n){
+  if (n == 0) {
+    return 0;
+  }
+  if (n == 1) {
+    return 1;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
+
+int main(){
+  int n = 0;
+  printf("Give a value for n\n");
+  scanf("%i", &n);
+  printf("The fibonnaci series\n");
+  for (int i = 0; i<=n; i++){
+    int tempValue = fib(i);
+    printf("%i\n", tempValue);
+  }
+}
